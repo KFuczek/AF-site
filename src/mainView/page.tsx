f22944391ba91c921910d5618a36a3c966e55b00
KFuczek <krzy96@interia.pl>
2025-04-29 17:08:26 +0200

    feat: test margin top

--- src/mainView/page.tsx
+++ src/mainView/page.tsx
@@ -6,18 +6,23 @@ import Paragraph from "@/views/paragraph/page";
 import Footer from "@/views/footer/page";
 import TopMenuBar from "@/views/TopMenuBar/page";
 import Header from "@/views/Header/page";
+import {useEffect} from "react";
 
 export default function View() {
 
+    useEffect(() => {
+        window.scrollBy(0, 20);
+    }, []);
+
     return (
         <div className={styles.main}>
-          <div className={`${styles.header}`}><Header /></div>
-          <div className={styles.topMenuBar}><TopMenuBar /></div>
-          <div className={styles.home}><Home /></div>
-          <div className={`${styles.paragraph}`}><Paragraph data={{title: 'title1', text: 'text1'}} /></div>
-          <div className={`${styles.paragraph}`}><Paragraph data={{title: 'title2', text: 'text2'}} /></div>
-          <div className={`${styles.paragraph}`}><Paragraph data={{title: 'title3', text: 'text3'}} /></div>
-          <div className={`${styles.footer}`}><Footer /></div>
+            <div className={`${styles.header}`}><Header/></div>
+            <div className={styles.topMenuBar}><TopMenuBar/></div>
+            <div className={styles.home}><Home/></div>
+            <div className={`${styles.paragraph}`}><Paragraph data={{title: 'title1', text: 'text1'}}/></div>
+            <div className={`${styles.paragraph}`}><Paragraph data={{title: 'title2', text: 'text2'}}/></div>
+            <div className={`${styles.paragraph}`}><Paragraph data={{title: 'title3', text: 'text3'}}/></div>
+            <div className={`${styles.footer}`}><Footer/></div>
         </div>
     );
 }
