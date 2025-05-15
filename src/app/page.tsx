2902cbf9ad660a1abc2a34bf7d1f4ec5378d77ef
KFuczek <krzy96@interia.pl>
2025-05-15 16:33:48 +0200

    feat: scroll button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,6 @@ export default function Home() {
     }, []);
 
     return isSSR ? null : (
-        View()
+        <View/>
     );
 }
--- src/components/up-button/page.tsx
+++ src/components/up-button/page.tsx
@@ -1,10 +1,49 @@
 import styles from './page.module.css';
+import {useEffect, useRef} from "react";
 
 export default function UpButton() {
+    const button = useRef<HTMLButtonElement>(null);
+    let throttleTimeout: NodeJS.Timeout | null = null;
+
+    useEffect(() => {
+        handleScroll();
+        document.body.addEventListener("scroll", throttledHandleScroll);
+
+        return () => {
+            document.body.removeEventListener("scroll", throttledHandleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        const scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
+
+        if (button.current) {
+            if (scrollPosition > 500) {
+                button.current.style.display = "block";
+            } else {
+                button.current.style.display = "none";
+            }
+        }
+    };
+
+    const throttledHandleScroll = () => {
+        if (throttleTimeout) {
+            return;
+        }
+
+        throttleTimeout = setTimeout(() => {
+            handleScroll();
+            throttleTimeout = null;
+        }, 200);
+    };
+
+    const scrollToTop = () => {
+        document.body.scrollTo({top: 0, behavior: "smooth"});
+    };
 
     return (
-        <a href={`#header`} className={styles.upButton}>
-            UP
-        </a>
+        <button ref={button} className={styles.upButton} onClick={scrollToTop} aria-label="Scroll to top">
+            ↑
+        </button>
     );
 }
